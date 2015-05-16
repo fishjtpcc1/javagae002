dbf69d3f7ab1bf4c86128e0542704efe35056f11
fishjtpcc1 <jtp.cc1@gmail.com>
2015-05-16 15:16:15 +0000

    console framework

--- src/main/java/jtp/cc1/ascsp1/MonsterGameServlet.java
+++ src/main/java/jtp/cc1/ascsp1/MonsterGameServlet.java
@@ -12,13 +12,25 @@ import javax.servlet.http.*;
 public class MonsterGameServlet extends HttpServlet {
   
   private static final long serialVersionUID = 1L; // know: because HttpServlet is serializable
+
+  class TheGame {
+    void TheGame() {
+      String this.data = "newgame ";
+    }
+  }
+  
   
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws java.io.IOException {
+    HttpSession mySession = req.getSession();
+    if (mySession.isNew()) {
+      mySession.setAttribute("theGame") = new TheGame();
+    }
     String input = req.getParameter("input");
     String method = "readln";
+    mySession.getAttribute("theGame").data += input + " ";
     resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"screen\": \"" + "you said:" + input + "\", \"method\": \"" + method + "\" }");
+    resp.getWriter().println("{ \"screen\": \"" + "you said:" + input + "<br>theGame:" + mySession.getAttribute("theGame").data + "\", \"method\": \"" + method + "\" }");
   }
 
 }
