3a88c98d972790453cbade2295bb6d02b5ec20f2
fishjtpcc1 <jtp.cc1@gmail.com>
2015-05-18 11:23:01 +0000

    basic gameplay

--- src/main/java/jtp/cc1/ascsp1/MonsterGameServlet.java
+++ src/main/java/jtp/cc1/ascsp1/MonsterGameServlet.java
@@ -96,7 +96,11 @@ public class MonsterGameServlet extends HttpServlet {
     String input = req.getParameter("input");
     // proceed with this use event
     Scene scene = (Scene)mySession.getAttribute("scene");
-    scene.handle(input);
+    if (scene != null) {
+      scene.handle(input);
+    } else {
+      screen = "session problem";
+    }
     // hand back to tier1 to present the new user state
     resp.setContentType("text/plain");
     resp.getWriter().println(json());
