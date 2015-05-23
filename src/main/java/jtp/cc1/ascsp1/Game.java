ee4e02e667ef33cc499afa57e5026875517f6aa6
fishjtpcc1 <jtp.biz@gmail.com>
2015-05-23 11:20:41 +0000

    scenes as classes - all smooth

--- src/main/java/jtp/cc1/ascsp1/Game.java
+++ src/main/java/jtp/cc1/ascsp1/Game.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
  */
 public class Game implements Serializable {
   private static final long serialVersionUID = 1L;
-  private int;
+  private int i;
   public String data;
   public String method = "read";
   public void restart() {
