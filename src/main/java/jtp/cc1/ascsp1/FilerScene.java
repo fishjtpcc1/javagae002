586efa80bd06cdeedf845ea4e4c1f8b194445e90
fishjtpcc1 <jtp.biz@gmail.com>
2015-06-03 13:31:09 +0000

    refactor to chained slave model

--- src/main/java/jtp/cc1/ascsp1/FilerScene.java
+++ src/main/java/jtp/cc1/ascsp1/FilerScene.java
@@ -13,7 +13,7 @@ public class FilerScene extends Scene implements Serializable {
   }
   
   public String draw() {
-    MonsterGameServlet.logWarning(datastore);
+    MonsterGameServlet.log.logWarning(datastore);
     Game[] savedGames = (Game[])datastore.getAttribute("savedGames");
     String rows = "";
     if (savedGames != null) {
